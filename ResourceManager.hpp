7da96d9c19b86621ebd8042ccad928c976d8d946
Dawid Knas <140855905+dawidkn@users.noreply.github.com>
2023-11-05 22:03:45 +0100

    Update ResourceManager.hpp
    LAB 2 Knas

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,32 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager() : resource_(std::make_unique<Resource>()) {}
+
+    double get() const { return resource_->get(); }
+
+    ResourceManager(const ResourceManager& other) : resource_(std::make_unique<Resource>(*other.resource_)) {}
+
+    ResourceManager(ResourceManager&& other) noexcept : resource_(std::move(other.resource_)) {}
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            resource_ = std::make_unique<Resource>(*other.resource_);
+        }
+        return *this;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        if (this != &other) {
+            resource_ = std::move(other.resource_);
+        }
+        return *this;
+    }
+
+private:
+    std::unique_ptr<Resource> resource_;
 };
+
